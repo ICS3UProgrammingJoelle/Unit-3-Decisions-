e67ec22b784f5d980ac04f884e5e56f99d54a0e2
Joelle Ishimwe <36892599+joelleishimwe@users.noreply.github.com>
2020-05-03 21:02:57 -0400

    updates

--- RunningAverageJor/RunningAverageJor/RunningAverageForm.Designer.cs
+++ RunningAverageJor/RunningAverageJor/RunningAverageForm.Designer.cs
@@ -29,14 +29,15 @@
         private void InitializeComponent()
         {
             this.btnCalculateAverage = new System.Windows.Forms.Button();
-            this.txtNumber = new System.Windows.Forms.TextBox();
-            this.lblEnterNumber = new System.Windows.Forms.Label();
+            this.txtNumber1 = new System.Windows.Forms.TextBox();
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.lblAverage = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnCalculateAverage
             // 
             this.btnCalculateAverage.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCalculateAverage.Location = new System.Drawing.Point(323, 177);
+            this.btnCalculateAverage.Location = new System.Drawing.Point(108, 215);
             this.btnCalculateAverage.Name = "btnCalculateAverage";
             this.btnCalculateAverage.Size = new System.Drawing.Size(176, 30);
             this.btnCalculateAverage.TabIndex = 0;
@@ -44,30 +45,41 @@
             this.btnCalculateAverage.UseVisualStyleBackColor = true;
             this.btnCalculateAverage.Click += new System.EventHandler(this.btnCalculateAverage_Click);
             // 
-            // txtNumber
+            // txtNumber1
             // 
-            this.txtNumber.Location = new System.Drawing.Point(580, 114);
-            this.txtNumber.Name = "txtNumber";
-            this.txtNumber.Size = new System.Drawing.Size(100, 20);
-            this.txtNumber.TabIndex = 1;
+            this.txtNumber1.Location = new System.Drawing.Point(518, 113);
+            this.txtNumber1.Name = "txtNumber1";
+            this.txtNumber1.Size = new System.Drawing.Size(100, 20);
+            this.txtNumber1.TabIndex = 1;
             // 
-            // lblEnterNumber
+            // lblNumber
             // 
-            this.lblEnterNumber.AutoSize = true;
-            this.lblEnterNumber.Font = new System.Drawing.Font("MS Reference Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblEnterNumber.Location = new System.Drawing.Point(15, 100);
-            this.lblEnterNumber.Name = "lblEnterNumber";
-            this.lblEnterNumber.Size = new System.Drawing.Size(464, 34);
-            this.lblEnterNumber.TabIndex = 2;
-            this.lblEnterNumber.Text = "Enter a number between 1 - 100";
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Font = new System.Drawing.Font("MS Reference Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumber.Location = new System.Drawing.Point(61, 93);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(374, 52);
+            this.lblNumber.TabIndex = 2;
+            this.lblNumber.Text = "Enter an average between 1 - 100\r\n(enter -1 to say goodbye)";
+            this.lblNumber.Click += new System.EventHandler(this.lblNumber_Click);
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.AutoSize = true;
+            this.lblAverage.Location = new System.Drawing.Point(534, 254);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(35, 13);
+            this.lblAverage.TabIndex = 3;
+            this.lblAverage.Text = "label1";
             // 
             // frmRunningAverage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.lblEnterNumber);
-            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.lblNumber);
+            this.Controls.Add(this.txtNumber1);
             this.Controls.Add(this.btnCalculateAverage);
             this.Name = "frmRunningAverage";
             this.Text = "Running Average                      by: Joelle Ishimwe";
@@ -79,8 +91,9 @@
         #endregion
 
         private System.Windows.Forms.Button btnCalculateAverage;
-        private System.Windows.Forms.TextBox txtNumber;
-        private System.Windows.Forms.Label lblEnterNumber;
+        private System.Windows.Forms.TextBox txtNumber1;
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.Label lblAverage;
     }
 }
 
--- RunningAverageJor/RunningAverageJor/RunningAverageForm.cs
+++ RunningAverageJor/RunningAverageJor/RunningAverageForm.cs
@@ -18,18 +18,58 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace RunningAverageJor
-{
+{           
     public partial class frmRunningAverage : Form
-    {
+    {    
+        //decalre variables
+        double sum = 0;
+        int numMarks = 0;
+        
         public frmRunningAverage()
         {
             InitializeComponent();
+            
+            //hide the answer label
+            this.lblAverage.Hide();
         }
 
         private void btnCalculateAverage_Click(object sender, EventArgs e)
         {
             //declare variables
+            double mark;
+            double average;
 
+            //set the variable to zero
+            mark = double.Parse(txtNumber1.Text);
+
+            //allows user to enter any number between 1-100
+            if (mark == -1)
+            {
+                MessageBox.Show("Running average ended", "Running Average");
+                this.btnCalculateAverage.Enabled = false;
+                this.txtNumber1.Enabled = false;
+            }
+            
+            //if user's enters -1 display goodbye message and disable buttons
+            else
+            {
+                //update the number of marks
+                numMarks++;
+
+                //update the sum
+                sum = sum + mark;
+
+                //calculate average
+                average = sum / numMarks;
+
+                //display answer
+                this.lblAverage.Show();
+                lblAverage.Text = Convert.ToString(average);
+            }
+        }
+
+        private void lblNumber_Click(object sender, EventArgs e)
+        {
 
         }
     }
